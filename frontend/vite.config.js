cd230e010aa61df652583d4857ca97a28f5ab98d
mojiri00 <moguricamera@gmail.com>
2025-11-19 21:31:29 +0900

    test: vitest 환경 설정 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +10,9 @@ export default defineConfig({
       },
     }),
   ],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.js'],
+    globals: true,
+  },
+});
